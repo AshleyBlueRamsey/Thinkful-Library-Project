39fa7cdbfbdf89321df095979a5769d5e35504e4
Ashley Ramsey <114588956+AshleyBlueRamsey@users.noreply.github.com>
2022-11-01 17:21:41 -0400

    Update books.js

--- public/src/books.js
+++ public/src/books.js
@@ -6,7 +6,14 @@ function findBookById(books, id) {  //searches the database and returns an array
   return books.find((book) => book.id.includes(id));
 }
 
-function partitionBooksByBorrowedStatus(books) {}
+//The following function named partitionBooksByBorrowedStatus returns two arrays inside of one
+//The first array contains books that are currently checked out
+//The second array contains books that have been returned
+function partitionBooksByBorrowedStatus(books) {  
+  const listOfBooksAvailable = books.filter((book) => book.borrows.returned === true);
+  const listOfBooksBorrowed = books.filter((book) => book.borrows.returned === false);
+  return [listOfBooksBorrowed, listOfBooksAvailable];
+}
 
 function getBorrowersForBook(book, accounts) {}
 
