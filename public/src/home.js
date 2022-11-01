6b425e09a499ab44bae80c4c14139921634bd77a
Ashley Ramsey <114588956+AshleyBlueRamsey@users.noreply.github.com>
2022-11-01 18:48:43 -0400

    Update home.js
    entered code for the function getMostCommonGenres and created a helper function named _topFiveMost Common. Tested code in Qualified. Kept getting an error for "unexpected ending" and realized that I forgot to close out the helper function with a " } " closing end of curly brackets. Updated code in GitHub and in Qualified, reran tests, and everything passed.

--- public/src/home.js
+++ public/src/home.js
@@ -16,7 +16,23 @@ function getBooksBorrowedCount(books) { //sorts through the database and returns
   return booksOnLoan.length;
 }
 
-function getMostCommonGenres(books) {}
+function _topFiveMostCommon(array) { //This is a helper function for the function named getMostCommonGenres and it returns the top 5 results
+  const results = array.sort((numA, numB) => (numA.count < numB.count ? 1 : -1)).slice(0,5); //Ensures that the results return no more than 5
+  return results;
+}
+  
+function getMostCommonGenres(books) { //Sorts through the database and returns an array with five or less names of the most common genres
+  const listOfMostFrequent = [];
+  for (let book of books) {
+    const genre = listOfMostFrequent.find((currentGenre) => currentGenre.name === book.genre);
+    if (genre) {
+      genre.count++;
+    } else {
+      listOfMostFrequent.push({ name: book.genre, count: 1});
+    }
+  }
+  return _topFiveMostCommon(listOfMostFrequent);
+}
 
 function getMostPopularBooks(books) {}
 
