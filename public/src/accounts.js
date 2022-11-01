10eef7d70266bec8d40db60094ab9a6e808d5961
Ashley Ramsey <114588956+AshleyBlueRamsey@users.noreply.github.com>
2022-11-01 13:00:06 -0400

    Update accounts.js

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -1,16 +1,25 @@
-function findAccountById(accounts, id) {
+function findAccountById(accounts, id) { //Searches through the array and finds the account that matches the requested id
   return accounts.find((account) => account.id.includes(id));
 }
-//Loops through the array and finds the account that matches the requested id
 
-function sortAccountsByLastName(accounts) {
+function sortAccountsByLastName(accounts) { //Sorts the accounts in the array by Last Name
   return accounts.sort((lastNameA, lastNameB) => lastNameA.name.last.toLowerCase() > lastNameB.name.last.toLowerCase() ? 1 : -1);
 }
-//Sorts the accounts in the array by Last Name
 
-function getTotalNumberOfBorrows(account, books) {}
 
-function getBooksPossessedByAccount(account, books, authors) {}
+function getTotalNumberOfBorrows(account, books) { //Searches through the array and returns the total number of books borrowed for each account
+  let result = 0;
+  const numberBorrowedById = books.forEach((book) => {
+    if (!!book.borrows) {
+      book.borrows.forEach((accounts) => {
+        if (accounts.id === account.id) {
+          result++;
+        }
+      });
+    }
+  });
+  return result;
+}
 
 module.exports = {
   findAccountById,
